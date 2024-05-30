3c15adb27e08c779735736eb1f7da213de5568e5
Michael St Clair <michaelst57@gmail.com>
2024-05-29 20:18:33 -0600

    initial setup

---
+++ lib/injexor.ex
@@ -0,0 +1,18 @@
+defmodule Injexor do
+  @moduledoc """
+  Documentation for `Injexor`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Injexor.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/injexor_test.exs
@@ -0,0 +1,8 @@
+defmodule InjexorTest do
+  use ExUnit.Case
+  doctest Injexor
+
+  test "greets the world" do
+    assert Injexor.hello() == :world
+  end
+end
